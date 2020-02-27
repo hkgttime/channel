fcc2fd378686469f2c4b5320d8f532804b84a6f8
513046827@qq.com <q7141627>
2020-02-27 14:34:51 +0800

    添加axios

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 import ElementUI from 'element-ui'
 
 Vue.config.productionTip = false
+Vue.property.$axios = axios
 Vue.use(ElementUI)
 new Vue({
   router,
